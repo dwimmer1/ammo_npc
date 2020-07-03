5b1965ffe325498b42a0548994fc171fe076e95c
ThatDani <67701825+ThatDani@users.noreply.github.com>
2020-07-03 10:38:54 +0200

    Update shared.lua

--- ammo_npc/shared.lua
+++ ammo_npc/shared.lua
@@ -1,21 +1,6 @@
 ENT.Type = "ai"
 ENT.Base = "base_ai"
 
-ENT.PrintName = "Ammo Npc"
+ENT.PrintName = "Ammo NPC"
 ENT.AutomaticFrameAdvance = true
 ENT.Spawnable = true
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
